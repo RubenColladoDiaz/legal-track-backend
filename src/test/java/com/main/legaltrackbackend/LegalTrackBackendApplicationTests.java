9822fb09fecfea1a3295caacf325435324b59a98
Your Name <you@example.com>
2026-04-13 13:43:06 +0200

    Initial commit

---
+++ src/test/java/com/main/legaltrackbackend/LegalTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.legaltrackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalTrackBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
